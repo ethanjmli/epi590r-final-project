cb286be44408f4c426e94365908c547ce31cc670
ethanjmli <ethan.jm.li@gmail.com>
2023-08-14 12:30:56 -0400

    Read 2014-2022 nba RAPTOR read historical nba RAPTOR

--- Test.R
+++ Test.R
@@ -1,4 +1,7 @@
 #Test file
 #Test
-nbadata <- read.csv(here::here("data","nba-raptor","latest_RAPTOR_by_team.csv"))
+nbadata.2023 <- read.csv(here::here("data","nba-raptor","latest_RAPTOR_by_team.csv"))
+nbadata.2014_2022 <- read.csv(here::here("data","nba-raptor","modern_RAPTOR_by_team.csv"))
+nbadata.historical <- read.csv(here::here("data","nba-raptor","historical_RAPTOR_by_team.csv"))
+
 
