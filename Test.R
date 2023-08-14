03a2a27194a59631634698ef00226655b1c212e1
ethanjmli <ethan.jm.li@gmail.com>
2023-08-14 12:33:31 -0400

    initialize dplyr arrange 2023 nba data by descending total war

--- Test.R
+++ Test.R
@@ -1,7 +1,9 @@
 #Test file
 #Test
+library(dplyr)
 nbadata.2023 <- read.csv(here::here("data","nba-raptor","latest_RAPTOR_by_team.csv"))
 nbadata.2014_2022 <- read.csv(here::here("data","nba-raptor","modern_RAPTOR_by_team.csv"))
 nbadata.historical <- read.csv(here::here("data","nba-raptor","historical_RAPTOR_by_team.csv"))
 
 
+nbadata.2023 <- nbadata.2023 %>% dplyr::arrange(desc(war_total))
